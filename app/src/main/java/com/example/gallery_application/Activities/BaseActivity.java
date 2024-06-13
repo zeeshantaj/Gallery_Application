848c22ebe7e4bf509f78fb5f8a069511711b4539
zeeshantaj <91092210+zeeshantaj@users.noreply.github.com>
2024-06-13 09:32:30 -0700

    Create BaseActivity.java

---
+++ app/src/main/java/com/example/gallery_application/Activities/BaseActivity.java
@@ -0,0 +1,49 @@
+package com.linkitsoft.L3CraftMenuboard.Activities;
+
+import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class BaseActivity extends AppCompatActivity {
+
+    // *********************************App View**************************************************************
+
+    private View core_view;
+
+    @Override
+    public void onWindowFocusChanged(boolean hasFocus) {
+        super.onWindowFocusChanged(hasFocus);
+        if (hasFocus) {
+            core_view.setSystemUiVisibility(hide_system_bars());
+        }
+    }
+
+    private int hide_system_bars() {
+        return View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                | View.SYSTEM_UI_FLAG_LAYOUT_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_HIDE_NAVIGATION
+                | View.SYSTEM_UI_FLAG_FULLSCREEN
+                | View.SYSTEM_UI_FLAG_IMMERSIVE_STICKY;
+    }
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        core_view = getWindow().getDecorView();
+        core_view.setOnSystemUiVisibilityChangeListener(new View.OnSystemUiVisibilityChangeListener() {
+            @Override
+            public void onSystemUiVisibilityChange(int visibility) {
+                if (visibility == 0) {
+                    core_view.setSystemUiVisibility(hide_system_bars());
+                }
+            }
+        });
+
+    }
+
+    // *********************************App View**************************************************************
+
+}
